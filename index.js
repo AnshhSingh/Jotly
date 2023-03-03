5e0f0001c29790ec246717bac888f007d2041b3d
AnshhSingh <anshsingh25bd@gmail.com>
2023-03-03 09:43:30 +0000

    added express js and login page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile("pages/app.html",{root: __dirname})
+  res.sendFile("pages/login.html",{root: __dirname})
 })
 app.get('/app', (req, res) => {
   res.sendFile("pages/app.html",{root: __dirname})
@@ -11,8 +11,8 @@ app.get('/app', (req, res) => {
 app.get('/script', (req, res) => {
   res.sendFile("script.js",{root: __dirname})
 })
-app.get('/style.css', (req, res) => {
-  res.sendFile("style.css",{root: __dirname})
+app.get('/style', (req, res) => {
+  res.sendFile("style.js",{root: __dirname})
 })
 
 app.listen(port, () => {
